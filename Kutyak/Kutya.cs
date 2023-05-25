1b24dfcb735345cbfb09a1ca8456650f60b7e317
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2023-05-25 13:54:04 +0200

    asd

--- Kutyak/Kutya.cs
+++ Kutyak/Kutya.cs
@@ -8,6 +8,25 @@ namespace Kutyak
 {
     internal class Kutya
     {
+        int id;
+        string kutyaFajtaja;
+        string kutyaNeve;
+        int kutyaEletkora;
+        string vizsgalatIdeje;
 
+        public Kutya(int id, string kutyaFajtaja, string kutyaNeve, int kutyaEletkora, string vizsgalatIdeje)
+        {
+            this.id = id;
+            this.kutyaFajtaja = kutyaFajtaja;
+            this.kutyaNeve = kutyaNeve;
+            this.kutyaEletkora = kutyaEletkora;
+            this.vizsgalatIdeje = vizsgalatIdeje;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string KutyaFajtaja { get => kutyaFajtaja; set => kutyaFajtaja = value; }
+        public string KutyaNeve { get => kutyaNeve; set => kutyaNeve = value; }
+        public int KutyaEletkora { get => kutyaEletkora; set => kutyaEletkora = value; }
+        public string VizsgalatIdeje { get => vizsgalatIdeje; set => vizsgalatIdeje = value; }
     }
 }
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -7,16 +7,31 @@ namespace Kutyak
         //public static List<string[]> kutyaNevek = new List<string[]>();
         public static Dictionary<int, string> kutyaNevek = new Dictionary<int, string>();
         public static Dictionary<int, string> kutyaFajtak = new Dictionary<int, string>();
+        public static List<Kutya> kutyaLista = new List<Kutya>();
 
         static void Main(string[] args)
         {
             Beolvasas("Kutyak", "kutyaNevek", "KutyaFajtak");
+            HanyKutyaNev(kutyaNevek);
+            KutyakAtlgEletkora(kutyaLista);
         }
 
-        private static void Beolvasas(string kutyak, string Nevek, string Fajtak)
+        private static void KutyakAtlgEletkora(List<Kutya> lista)
         {
-            File.ReadAllLines(Nevek, Encoding.UTF8).ToList().ForEach(x => { kutyaNevek.Add(Convert.ToInt16(x.Split(" ")[0]), x.Split(" ")[1]); });
-            File.ReadAllLines(Fajtak, Encoding.UTF8).ToList().ForEach(x => { kutyaFajtak.Add(Convert.ToInt16(x.Split(" ")[0]), x.Split(" ")[1]); });
+            Console.WriteLine($"6. feladat: {lista.Average(x => x.KutyaEletkora)}");
+        }
+
+        private static void HanyKutyaNev(Dictionary<int, string> kutyaNevek)
+        {
+            Console.WriteLine($"3. feladat: {kutyaNevek.Count}");
+        }
+
+        private static void Beolvasas(string kutyakEleres, string Nevek, string Fajtak)
+        {
+            File.ReadAllLines(Nevek, Encoding.UTF8).ToList().ForEach(x => { kutyaNevek.Add(Convert.ToInt16(x.Split(";")[0]), x.Split(";")[1]); });
+            File.ReadAllLines(Fajtak, Encoding.UTF8).ToList().ForEach(x => { kutyaFajtak.Add(Convert.ToInt16(x.Split(";")[0]), x.Split(";")[1]); });
+            File.ReadAllLines(kutyakEleres, Encoding.UTF8).ToList().ForEach(x => { kutyaLista.Add(new Kutya(Convert.ToInt16(x.Split(";")[0]),kutyaFajtak[Convert.ToInt16(x.Split(";")[1])],
+                kutyaNevek[Convert.ToInt16(x.Split(";")[2])], Convert.ToInt16(x.Split(";")[3]), x.Split(";")[4])); });
         }
     }
 }
