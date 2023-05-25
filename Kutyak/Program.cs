cc0d27d69266ef8a06b4c49d80abe00ba17f037f
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2023-05-25 12:52:23 +0200

    Ebéd után

--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -1,16 +1,22 @@
-﻿namespace Kutyak
+﻿using System.Text;
+
+namespace Kutyak
 {
     internal class Program
     {
+        //public static List<string[]> kutyaNevek = new List<string[]>();
+        public static Dictionary<int, string> kutyaNevek = new Dictionary<int, string>();
+        public static Dictionary<int, string> kutyaFajtak = new Dictionary<int, string>();
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Beolvasas(""));
+            Beolvasas("Kutyak", "kutyaNevek", "KutyaFajtak");
         }
 
-        private static void Beolvasas(string eleresiUt)
+        private static void Beolvasas(string kutyak, string Nevek, string Fajtak)
         {
-            
+            File.ReadAllLines(Nevek, Encoding.UTF8).ToList().ForEach(x => { kutyaNevek.Add(Convert.ToInt16(x.Split(" ")[0]), x.Split(" ")[1]); });
+            File.ReadAllLines(Fajtak, Encoding.UTF8).ToList().ForEach(x => { kutyaFajtak.Add(Convert.ToInt16(x.Split(" ")[0]), x.Split(" ")[1]); });
         }
     }
 }
